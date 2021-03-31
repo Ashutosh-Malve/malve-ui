858342a50d83b63df89cd31f46b04b387fa9f7bb
Ashutosh <ashutoshmalve@hotmail.com>
2021-03-31 17:30:12 +0530

    new fix

--- pages/api/article.js
+++ pages/api/article.js
@@ -3,7 +3,7 @@ const Article = (_, res) => {
   res.json({
     id: 1,
     author: 'Ashutosh Malve',
-    company: 'MalveOrg',
+    company: 'Malve.Org',
     image_url: 'https://miro.medium.com/fit/c/262/262/1*A_obp7pGgHUULd1-FxZwEQ.png',
     content: "How do I learn machine learning '?'"
   })
